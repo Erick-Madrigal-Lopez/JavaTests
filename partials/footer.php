c60b2c4c3f69112c417530f0c827996314633302
Erick-Madrigal-Lopez <erick.egml2000@gmail.com>
2022-10-18 12:07:29 -0500

    Modificación al enlace del correo

--- partials/footer.php
+++ partials/footer.php
@@ -16,7 +16,7 @@
 						Aviso de privacidad
 					</a>
 					<p class="text-white small f600">
-						<a href="mailto:contacto@asersa.mx" class="text-white f600">contacto@asersa.mx</a> <br><br>
+						<a href="mailto:contacto@asersa.mx" class="text-decoration-none text-white f600">contacto@asersa.mx</a> <br><br>
 						<a href="tel:3336960808" class="text-decoration-none text-white">+52 (33) 3696 0808</a>
 					</p>
 				</div>
