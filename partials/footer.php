c5c3e730790d49b18e20ca5b3b3d4e559f936d74
Erick-Madrigal-Lopez <erick.egml2000@gmail.com>
2022-10-21 17:05:42 -0500

    sans-serif a style, customstyles, footer

--- partials/footer.php
+++ partials/footer.php
@@ -14,10 +14,10 @@
 				<div class="col-12 col-lg-3 mb-3 mb-lg-0 text-center text-md-left">
 					<div class="d-flex justify-content-end">
 						<p class="text-white small sans-serif">
-							<a href="aviso-de-privacidad.php" class="text-white text-decoration-none mb-1 d-block">Aviso de privacidad</a>
-							<a href="mailto:emmanuel.villasenor@asersa.mx" class="text-decoration-none text-white mb-1 text-lowercase">emmanuel.villasenor@asersa.mx</a><br>
-							<a href="tel:3336960808" class="text-decoration-none text-white">+52 (33) 3696 0808</a> Ext. 11002</p>
-					</div>
+							<a href="aviso-de-privacidad.php" class="text-white text-decoration-none mb-1 d-block sans-serif">Aviso de privacidad</a>
+							<a href="mailto:emmanuel.villasenor@asersa.mx" class="text-decoration-none text-white mb-1 text-lowercase sans-serif">emmanuel.villasenor@asersa.mx</a><br>
+							<a href="tel:3336960808" class="text-decoration-none text-white sans-serif">+52 (33) 3696 0808</a> Ext. 11002</p>
+					</div> 
 				</div>
 				<div class="col-12 col-lg-2 mb-3 mb-lg-0 redes-footer text-center text-md-left">
 					<p class="text-white small f600 mb-2">
